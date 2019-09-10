b45f5560e55aea17febd3e99e87d7d8d052b8a73
Matheus Negrao <matheus.dnegrao@gmail.com>
2019-09-10 11:37:53 -0300

    :truck: little changes

--- src/finiteAutomata.py
+++ src/finiteAutomata.py
@@ -1,10 +1,20 @@
-
-
 class finiteAutomata(object):
+    # default settings
     __initial_state = 0
     __error_state = -1
     __alphabet = []
     __FA = {}
 
+
+    __input_folder = 'set'
+
+    __tokens_file = 'tokens.csv'
+    __gramma_file = ''
+
     def __init__(self, file):
-        self.__mapGramma(file)
+        self.mapGramma(file)
+        self.mapTokens()
+    
+    def mapGramma(file):
+
+    def mapTokens(file):    
