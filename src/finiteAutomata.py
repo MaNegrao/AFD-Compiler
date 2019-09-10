c8e6f93cda9756166e1dac5d2adcc1c9eb8196fa
Matheus Negrao <matheus.dnegrao@gmail.com>
2019-09-10 11:17:49 -0300

    :recycle: rebuilding finite automata code

--- src/finiteAutomata.py
+++ src/finiteAutomata.py
@@ -1,2 +1,10 @@
-import csv
 
+
+class finiteAutomata(object):
+    __initial_state = 0
+    __error_state = -1
+    __alphabet = []
+    __FA = {}
+
+    def __init__(self, file):
+        self.__mapGramma(file)
