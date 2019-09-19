e57034a461183313ba9cc59b6c26577c9fe86d52
Matheus Negrao <matheus.dnegrao@gmail.com>
2019-09-18 21:18:17 -0300

    :construction: modifying class calls in "main" archive

--- compile.py
+++ compile.py
@@ -2,6 +2,6 @@ from src.LexicalAnalyzer import LexicalAnalyzer
 from src.FiniteAutomata import FiniteAutomata
 
 fa = FiniteAutomata()
-symbol_table = LexicalAnalyzer()
+symbol_table = LexicalAnalyzer(fa)
 if symbol_table is None:
     print('\nAborted!')
